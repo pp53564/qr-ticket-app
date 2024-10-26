1228d832090d5b0ced306791455e9f4289d22b43
petra <pp53564@fer.hr>
2024-10-26 15:37:25 +0200

    env fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL,
+  },
 }
 
 export default nextConfig;
